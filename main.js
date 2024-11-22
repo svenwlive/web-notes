689fef9f097174cd68b80d889015d3e06292a679
Svenw <74722783+svenwlive@users.noreply.github.com>
2024-11-22 11:17:21 +0200

    update

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-console.info("hiya! your javascript is working!")
-console.info("^_^")
+console.info("hiya! your javascript is working!");
+console.info("^_^");
+
+const createnewnote = document.querySelector('.createnewbtn');
+
+createnewnote.addEventListener("click", function (e) {
+    console.info("a new note will be created")
+    let newtitle = prompt("Enter a title")
+    console.log("new title: " + newtitle)
+})
